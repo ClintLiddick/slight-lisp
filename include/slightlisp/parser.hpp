0277cbde13695a9bc7d0c786462f6d973f0b4dcc
Clint Liddick <clint.liddick@gmail.com>
2016-06-07 22:53:10 -0400

    parser and tests

--- include/slightlisp/parser.hpp
+++ include/slightlisp/parser.hpp
@@ -1,7 +1,105 @@
 #ifndef SLIGHTLISP_PARSER_HPP
 #define SLIGHTLISP_PARSER_HPP
 
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
+#include <sstream>
 #include <string>
+#include <vector>
 
+#include <slightlisp/types.hpp>
+
+namespace slightlisp
+{
+using string = std::string;
+
+string replace_all(const string& input, const string& from, const string& to)
+{
+  string spaced{input};
+  if (input.empty()) return spaced;
+
+  size_t pos = 0;
+  while ((pos = spaced.find(from, pos)) != std::string::npos) {
+    spaced.replace(pos, from.length(), to);
+    pos += to.length();
+  }
+
+  return spaced;
+}
+
+std::vector<string> tokenize(const string& raw)
+{
+  string spaced = replace_all(replace_all(raw, "(", " ( "), ")", " ) ");
+  std::istringstream buffer{spaced};
+  std::vector<string> tokens{std::istream_iterator<string>(buffer),
+                             std::istream_iterator<string>()};
+  return tokens;
+}
+
+namespace detail
+{
+inline bool is_zero_value(string token)
+{
+  if (token.size() == 1) {
+    return token.substr(0, 1) == "0";
+  } else if (token.size() == 3) {
+    return token.substr(0, 3) == "0.0";
+  } else {
+    return false;
+  }
+}
+
+TypePtr parse_token(string token)
+{
+  Numeric n = std::atof(token.c_str());
+  if (n == 0) {
+    // determine if the value actually was zero
+    // or was just unable to be parsed
+    if (is_zero_value(token)) {
+      return std::move(std::make_unique<Type>(n));
+    } else {
+      return std::move(std::make_unique<Type>(token));
+    }
+  }
+  return std::move(std::make_unique<Type>(n));
+}
+
+TypePtr parse(std::vector<string>& tokens)
+{
+  string token = tokens[tokens.size() - 1];
+  tokens.pop_back();
+
+  // parse list
+  if (token == "(") {
+    TypePtr list = std::make_unique<Type>();
+    while (tokens[tokens.size() - 1] != ")") {
+      list->list.push_back(parse(tokens));
+    }
+    tokens.pop_back();  // pop ')'
+    return std::move(list);
+  }
+  // bad parse
+  else if (token == ")") {
+    throw syntax_exception{syntax_exception::UNEXPECTED_PARENS};
+  }
+  // parse atomic
+  else {
+    return std::move(parse_token(token));
+  }
+}
+
+}  // detail
+
+TypePtr parse(std::vector<string>& tokens)
+{
+  if (tokens.size() == 0) {
+    throw syntax_exception{syntax_exception::EMPTY_EXPRESSION};
+  }
+  std::reverse(tokens.begin(), tokens.end());
+  return detail::parse(tokens);
+}
+
+}  // slightlisp
 
 #endif  // SLIGHTLISP_PARSER_HPP
--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,5 +1,235 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-TEST(ParserTests, TestTest) {
-  ASSERT_TRUE(true);
+#include <stack>
+#include <slightlisp/parser.hpp>
+#include <slightlisp/types.hpp>
+
+using namespace slightlisp;
+
+using string = std::string;
+using ::testing::ContainerEq;
+
+TEST(ReplaceAllTests, SingleChar)
+{
+  string start = "abc";
+  ASSERT_EQ(replace_all(start, "b", "z"), "azc");
+}
+
+TEST(ReplaceAllTests, ShorterWithLonger)
+{
+  string start = "abc";
+  ASSERT_EQ(replace_all(start, "b", "1234"), "a1234c");
+}
+
+TEST(ReplaceAllTests, LongerWithShorter)
+{
+  string start = "abc";
+  ASSERT_EQ(replace_all(start, "ab", "z"), "zc");
+}
+
+TEST(ReplaceAllTests, ReplaceMultiple)
+{
+  string start = "abcabc";
+  ASSERT_EQ(replace_all(start, "a", "z"), "zbczbc");
+}
+
+TEST(ReplaceAllTests, ReplaceConsecutive)
+{
+  string start = "aa";
+  ASSERT_EQ(replace_all(start, "a", "z"), "zz");
+}
+
+TEST(ReplaceAllTests, ReplaceWithSpace)
+{
+  string start = "abcabc";
+  ASSERT_EQ(replace_all(start, "a", " "), " bc bc");
+}
+
+TEST(ReplaceAllTests, ReplaceWithSimilar)
+{
+  string start = "abcabc";
+  ASSERT_EQ(replace_all(start, "a", " a"), " abc abc");
+}
+
+TEST(ReplaceAllTests, EmptyFromString)
+{
+  ASSERT_EQ(replace_all("", "a", "1"), "");
+}
+
+TEST(ReplaceAllTests, NestedCalls)
+{
+  ASSERT_EQ(replace_all(replace_all("abc", "a", "1"), "b", "2"), "12c");
+}
+
+/***************************************************************************/
+
+TEST(TokenizeTests, OneToken)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("blah");
+  ASSERT_THAT(tokenize("blah"), ContainerEq(tokens));
+}
+
+TEST(TokenizeTests, TwoTokens)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("+");
+  tokens.push_back("-");
+  ASSERT_THAT(tokenize("+ -"), ContainerEq(tokens));
+}
+
+TEST(TokenizeTests, EmptyParens)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("(");
+  tokens.push_back(")");
+  ASSERT_THAT(tokenize("()"), ContainerEq(tokens));
+}
+
+TEST(TokenizeTests, UnspacedList)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("(");
+  tokens.push_back("a");
+  tokens.push_back(")");
+  ASSERT_THAT(tokenize("(a)"), ContainerEq(tokens));
+}
+
+TEST(TokenizeTests, SpacedList)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("(");
+  tokens.push_back("a");
+  tokens.push_back(")");
+  ASSERT_THAT(tokenize("( a )"), ContainerEq(tokens));
+}
+
+TEST(TokenizeTests, FloatingPointTokens)
+{
+  std::vector<std::string> tokens;
+  tokens.push_back("1.0");
+  tokens.push_back("1.5");
+  ASSERT_THAT(tokenize("1.0 1.5"), ContainerEq(tokens));
+}
+
+/***************************************************************************/
+
+TEST(TypeTests, ListDefaultConstruction)
+{
+  Type t;
+  ASSERT_EQ(Type::LIST, t.tag);
+}
+
+TEST(TypeTests, SymbolConstruction)
+{
+  Type t{"str"};
+  ASSERT_EQ(Type::SYMBOL, t.tag);
+}
+
+TEST(TypeTests, NumericEquality)
+{
+  Type t1{1.0}, t2{1.0};
+  ASSERT_EQ(t1, t2);
+}
+
+TEST(TypeTests, SymbolEquality)
+{
+  Type t1{"test"}, t2{"test"};
+  ASSERT_EQ(t1, t2);
+}
+
+TEST(TypeTests, ListEquality)
+{
+  Type t1, t2;
+  t1.list.push_back(std::make_unique<Type>("t1"));
+  t1.list.push_back(std::make_unique<Type>("t2"));
+  t2.list.push_back(std::make_unique<Type>("t1"));
+  t2.list.push_back(std::make_unique<Type>("t2"));
+  ASSERT_EQ(t1, t2);
+}
+
+
+/***************************************************************************/
+
+TEST(ParseTests, Empty)
+{
+  auto tokens = tokenize("");
+  ASSERT_THROW(parse(tokens), syntax_exception);
+}
+
+TEST(ParseTests, NoParens)
+{
+  auto tokens = tokenize("1");
+  TypePtr parsed = parse(tokens);
+  TypePtr one = std::make_unique<Type>(1);
+  ASSERT_EQ(*parsed, *one);
+}
+
+TEST(ParseTests, EmptyList)
+{
+  auto tokens = tokenize("()");
+  TypePtr parsed = parse(tokens);
+  Type list;
+  ASSERT_EQ(*parsed, list);
+}
+
+TEST(ParseTests, ListOfIntNumerics)
+{
+  auto tokens = tokenize("(1 2 3)");
+  Type list;
+  list.list.push_back(std::make_unique<Type>(1));
+  list.list.push_back(std::make_unique<Type>(2));
+  list.list.push_back(std::make_unique<Type>(3));
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, list);
+}
+
+TEST(ParseTests, ListOfFloatNumerics)
+{
+  auto tokens = tokenize("(1.0 2.0 3.0)");
+  Type list;
+  list.list.push_back(std::make_unique<Type>(1.0));
+  list.list.push_back(std::make_unique<Type>(2.0));
+  list.list.push_back(std::make_unique<Type>(3.0));
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, list);
+}
+
+TEST(ParseTests, Zero)
+{
+  auto tokens = tokenize("0.0");
+  Type zero{0};
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, zero);
+}
+
+TEST(ParseTests, DISABLED_LongZeroString)
+{
+  auto tokens = tokenize("00");
+  Type zero{0};
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, zero) << "This is a documented deficiency in the parser";
+}
+
+TEST(ParseTests, ListOfSymbols)
+{
+  auto tokens = tokenize("(first second third)");
+  Type list;
+  list.list.push_back(std::make_unique<Type>("first"));
+  list.list.push_back(std::make_unique<Type>("second"));
+  list.list.push_back(std::make_unique<Type>("third"));
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, list);
+}
+
+TEST(ParseTests, NestedLists)
+{
+  auto tokens = tokenize("(first (second))");
+  Type outer_list, inner_list;
+  outer_list.list.push_back(std::make_unique<Type>("first"));
+  inner_list.list.push_back(std::make_unique<Type>("second"));
+  outer_list.list.push_back(std::make_unique<Type>(std::move(inner_list)));
+  TypePtr parsed = parse(tokens);
+  ASSERT_EQ(*parsed, outer_list);
 }
