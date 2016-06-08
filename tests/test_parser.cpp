f709e1d1c7c50c1a0ea3c405e557ba0bce4ad74c
Clint Liddick <clint.liddick@gmail.com>
2016-06-07 23:35:12 -0400

    move type tests into own file

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -10,6 +10,8 @@ using namespace slightlisp;
 using string = std::string;
 using ::testing::ContainerEq;
 
+/***************************************************************************/
+
 TEST(ReplaceAllTests, SingleChar)
 {
   string start = "abc";
@@ -115,78 +117,6 @@ TEST(TokenizeTests, FloatingPointTokens)
 
 /***************************************************************************/
 
-TEST(TypeTests, ListDefaultConstruction)
-{
-  Type t;
-  ASSERT_EQ(Type::LIST, t.tag);
-}
-
-TEST(TypeTests, SymbolConstruction)
-{
-  Type t{"str"};
-  ASSERT_EQ(Type::SYMBOL, t.tag);
-}
-
-TEST(TypeTests, NumericEquality)
-{
-  Type t1{1.0}, t2{1.0};
-  ASSERT_EQ(t1, t2);
-}
-
-TEST(TypeTests, SymbolEquality)
-{
-  Type t1{"test"}, t2{"test"};
-  ASSERT_EQ(t1, t2);
-}
-
-TEST(TypeTests, ListEquality)
-{
-  Type t1, t2;
-  t1.list.push_back(std::make_unique<Type>("t1"));
-  t1.list.push_back(std::make_unique<Type>("t2"));
-  t2.list.push_back(std::make_unique<Type>("t1"));
-  t2.list.push_back(std::make_unique<Type>("t2"));
-  ASSERT_EQ(t1, t2);
-}
-
-
-TEST(TypeTests, NumericToString)
-{
-  Type t{1};
-  ASSERT_EQ(t.to_string(), "1");
-}
-
-TEST(TypeTests, SymbolToString)
-{
-  Type t{"a"};
-  ASSERT_EQ(t.to_string(), "a");
-}
-
-TEST(TypeTests, EmptyListToString)
-{
-  Type t;
-  ASSERT_EQ(t.to_string(), "()");
-}
-
-TEST(TypeTests, SingleItemString)
-{
-  Type t;
-  t.list.push_back(std::make_unique<Type>(1));
-  ASSERT_EQ(t.to_string(), "(1)");
-}
-
-TEST(TypeTests, MultiItemString)
-{
-  Type t;
-  t.list.push_back(std::make_unique<Type>(1));
-  t.list.push_back(std::make_unique<Type>(2));
-  t.list.push_back(std::make_unique<Type>(3));
-  ASSERT_EQ(t.to_string(), "(1 2 3)");
-}
-
-
-/***************************************************************************/
-
 TEST(ParseTests, Empty)
 {
   auto tokens = tokenize("");
@@ -268,4 +198,3 @@ TEST(ParseTests, NestedLists)
   TypePtr parsed = parse(tokens);
   ASSERT_EQ(*parsed, outer_list);
 }
-
